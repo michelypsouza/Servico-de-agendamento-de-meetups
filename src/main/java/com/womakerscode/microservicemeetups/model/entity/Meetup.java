835d18add0f2649046bbb1d098cc6b7ff047126d
michelypsouza <michely.psouza@gmail.com>
2022-04-11 23:00:08 -0300

    criando entity meetup

---
+++ src/main/java/com/womakerscode/microservicemeetups/model/entity/Meetup.java
@@ -0,0 +1,35 @@
+package com.womakerscode.microservicemeetups.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Meetup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String event;
+
+    @JoinColumn
+    @ManyToOne
+    private Registration registration;
+
+    @Column
+    private LocalDate meetupDate;
+
+    @Column
+    private Boolean registered;
+
+}
