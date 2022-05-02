a27b5936f85986aa8fb7bf41ee8e95628a8707ea
michelypsouza <michely.psouza@gmail.com>
2022-05-01 21:54:38 -0300

    correção dos testes de RegistrationService

--- src/test/java/com/womakerscode/microservicemeetups/service/RegistrationServiceTest.java
+++ src/test/java/com/womakerscode/microservicemeetups/service/RegistrationServiceTest.java
@@ -4,7 +4,6 @@ import com.womakerscode.microservicemeetups.exception.BusinessException;
 import com.womakerscode.microservicemeetups.model.entity.Event;
 import com.womakerscode.microservicemeetups.model.entity.Registration;
 import com.womakerscode.microservicemeetups.model.enumeration.EventTypeEnum;
-import com.womakerscode.microservicemeetups.repository.EventRepository;
 import com.womakerscode.microservicemeetups.repository.RegistrationRepository;
 import com.womakerscode.microservicemeetups.service.impl.RegistrationServiceImpl;
 import org.assertj.core.api.Assertions;
@@ -14,10 +13,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
@@ -33,9 +37,6 @@ public class RegistrationServiceTest {
     @MockBean
     RegistrationRepository registrationRepository;
 
-    @MockBean
-    EventRepository eventRepository;
-
     @BeforeEach
     public void setUp() {
         this.registrationService = new RegistrationServiceImpl(registrationRepository);
@@ -46,9 +47,7 @@ public class RegistrationServiceTest {
     public void saveRegistration() throws BusinessException {
 
         // cenario
-        Registration registration = createValidRegistration(persistEvent());
-
-        //Optional<Event> returnedRegistration = eventRepository.findById(registration.getEvent().getId());
+        Registration registration = createValidRegistration(createValidEvent());
 
         // execucao
         Mockito.when(registrationRepository.findExistingRegistrationEvent(Mockito.anyLong(), Mockito.anyLong()))
@@ -60,8 +59,9 @@ public class RegistrationServiceTest {
         // assert
         assertThat(savedRegistration.getId()).isEqualTo(101);
         assertThat(savedRegistration.getNameTag()).isEqualTo("Michely Souza");
-        assertThat(savedRegistration.getDateOfRegistration()).isEqualTo(LocalDateTime.now());
-//        assertThat(savedRegistration.getRegistrationNumber()).isEqualTo("001");
+        assertThat(savedRegistration.getEvent()).isEqualTo(registration.getEvent());
+        assertThat(savedRegistration.getDateOfRegistration()).isEqualTo(registration.getDateOfRegistration());
+        assertThat(savedRegistration.getParticipantId()).isEqualTo(registration.getParticipantId());
 
     }
 
@@ -69,7 +69,7 @@ public class RegistrationServiceTest {
     @DisplayName("Should throw business error when try to save a new registration duplicated")
     public void shouldNotSaveAsRegistrationDuplicated() {
 
-        Registration registration = createValidRegistration(persistEvent());
+        Registration registration = createValidRegistration(createValidEvent());
         Mockito.when(registrationRepository.findExistingRegistrationEvent(Mockito.anyLong(), Mockito.anyLong()))
                 .thenReturn(Optional.of(registration));
 
@@ -87,20 +87,18 @@ public class RegistrationServiceTest {
     public void getByRegistrationIdTest() {
 
         // cenario
-//        Long id = 11L;
-        Registration registration = createValidRegistration(persistEvent());
-        Long id = registration.getId();
-        registration.setId(id);
-        Mockito.when(registrationRepository.findById(id)).thenReturn(Optional.of(registration));
+        Registration registration = createValidRegistration(createValidEvent());
+        Mockito.when(registrationRepository.findById(registration.getId())).thenReturn(Optional.of(registration));
 
         // execucao
-        Optional<Registration> foundRegistration = registrationService.getRegistrationById(id);
+        Optional<Registration> foundRegistration = registrationService.getRegistrationById(registration.getId());
 
         assertThat(foundRegistration.isPresent()).isTrue();
-        assertThat(foundRegistration.get().getId()).isEqualTo(id);
+        assertThat(foundRegistration.get().getId()).isEqualTo(registration.getId());
         assertThat(foundRegistration.get().getNameTag()).isEqualTo(registration.getNameTag());
+        assertThat(foundRegistration.get().getEvent()).isEqualTo(registration.getEvent());
         assertThat(foundRegistration.get().getDateOfRegistration()).isEqualTo(registration.getDateOfRegistration());
-//        assertThat(foundRegistration.get().getRegistrationNumber()).isEqualTo(registration.getRegistrationNumber());
+        assertThat(foundRegistration.get().getParticipantId()).isEqualTo(registration.getParticipantId());
 
     }
 
@@ -129,88 +127,67 @@ public class RegistrationServiceTest {
 
     }
 
-//    @Test
-//    @DisplayName("Should update an registration")
-//    public  void updateRegistration() {
-//
-//        // cenario
-//        Integer id = 11;
-//        Registration updatingRegistration = Registration.builder().id(11).build();
-//
-//        // execucao
-//        Registration updatedRegisttration = createValidRegistration();
-//        updatedRegisttration.setId(id);
-//
-//        Mockito.when(registrationRepository.save(updatingRegistration)).thenReturn(updatedRegisttration);
-//        Registration registration = registrationService.update(updatingRegistration);
-//
-//        // assert
-//        assertThat(registration.getId()).isEqualTo(updatedRegisttration.getId());
-//        assertThat(registration.getName()).isEqualTo(updatedRegisttration.getName());
-//        assertThat(registration.getDateOfRegistration()).isEqualTo(updatedRegisttration.getDateOfRegistration());
-//        assertThat(registration.getRegistrationNumber()).isEqualTo(updatedRegisttration.getRegistrationNumber());
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Should filter registrations must by properties")
-//    public void findRegistrationTest() {
-//
-//        // cenario
-//        Registration registration = createValidRegistration();
-//        PageRequest pageRequest = PageRequest.of(0, 10);
-//
-//        List<Registration> listRegistrations = List.of(registration);
-//        Page<Registration> page = new PageImpl<>(List.of(registration),
-//                PageRequest.of(0,10), 1);
-//
-//        // execucao
-//        Mockito.when(registrationRepository.findAll(Mockito.any(Example.class), Mockito.any(PageRequest.class)))
-//                .thenReturn(page);
-//
-//        Page<Registration> result = registrationService.find(registration, pageRequest);
-//
-//        // assert
-//        assertThat(result.getTotalElements()).isEqualTo(1);
-//        assertThat(result.getContent()).isEqualTo(listRegistrations);
-//        assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
-//        assertThat(result.getPageable().getPageSize()).isEqualTo(10);
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Should get an Registration model by registration attribute")
-//    public void getRegistrationByRegistrationAttribute() {
-//
-//        String registrationAttribute = "1234";
-//
-//        Mockito.when(registrationRepository.findByRegistration(registrationAttribute))
-//                .thenReturn(Optional.of(Registration.builder().id(11).registrationNumber(registrationAttribute).build()));
-//
-//        Optional<Registration> registration = registrationService
-//                .getRegistrationByRegistrationNumber(registrationAttribute);
-//
-//        assertThat(registration.isPresent()).isTrue();
-//        assertThat(registration.get().getId()).isEqualTo(11);
-//        assertThat(registration.get().getRegistrationNumber()).isEqualTo(registrationAttribute);
-//
-//        Mockito.verify(registrationRepository, Mockito.times(1))
-//                .findByRegistration(registrationAttribute);
-//
-//    }
+    @Test
+    @DisplayName("Should update an registration")
+    public  void updateRegistration() {
 
-    private Event persistEvent() {
+        // cenario
+        Long id = 28L;
+        Registration updatingRegistration = Registration.builder().id(28L).build();
+
+        // execucao
+        Registration updatedRegistration = createValidRegistration(createValidEvent());
+        updatedRegistration.setId(id);
+
+        Mockito.when(registrationRepository.save(updatingRegistration)).thenReturn(updatedRegistration);
+        Registration registration = registrationService.update(updatingRegistration);
+
+        // assert
+        assertThat(registration.getId()).isEqualTo(updatedRegistration.getId());
+        assertThat(registration.getNameTag()).isEqualTo(updatedRegistration.getNameTag());
+        assertThat(registration.getEvent()).isEqualTo(updatedRegistration.getEvent());
+        assertThat(registration.getDateOfRegistration()).isEqualTo(updatedRegistration.getDateOfRegistration());
+        assertThat(registration.getParticipantId()).isEqualTo(updatedRegistration.getParticipantId());
+
+    }
+
+    @Test
+    @DisplayName("Should filter registrations must by properties")
+    public void findRegistrationTest() {
+
+        // cenario
+        Registration registration = createValidRegistration(createValidEvent());
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        List<Registration> listRegistrations = List.of(registration);
+        Page<Registration> page = new PageImpl<>(List.of(registration),
+                PageRequest.of(0,10), 1);
+
+        // execucao
+        Mockito.when(registrationRepository.findAll(Mockito.any(Example.class), Mockito.any(PageRequest.class)))
+                .thenReturn(page);
+
+        Page<Registration> result = registrationService.find(registration, pageRequest);
+
+        // assert
+        assertThat(result.getTotalElements()).isEqualTo(1);
+        assertThat(result.getContent()).isEqualTo(listRegistrations);
+        assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+        assertThat(result.getPageable().getPageSize()).isEqualTo(10);
+
+    }
+
+    private Event createValidEvent() {
         Long numberRandom = new Random().nextLong();
-        Event event = Event.builder()
-//                .id(numberRandom)
-                .title("Encontro Mulheres e Carreira em Tecnologia "+numberRandom.toString())
+        return Event.builder()
+                .id(numberRandom)
+                .title("Encontro Mulheres e Carreira em Tecnologia " + numberRandom.toString())
                 .description("Mulheres e Carreira em Tecnologia parceria WoMakersCode e Zé Delivery")
                 .startDate(LocalDateTime.of(2022, 3, 24, 19, 0))
                 .endDate(LocalDateTime.of(2022, 3, 24, 21, 0))
                 .eventTypeEnum(EventTypeEnum.FACE_TO_FACE)
                 .organizerId(3L)
                 .build();
-        return eventRepository.save(event);
     }
 
     private Registration createValidRegistration(Event event) {
