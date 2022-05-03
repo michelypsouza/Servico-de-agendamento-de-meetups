5cbbd42fe44cfd0b3b6bf6bdc053570de37f3936
michelypsouza <michely.psouza@gmail.com>
2022-05-02 22:23:10 -0300

    Ajustes dos imports

--- src/main/java/com/womakerscode/microservicemeetups/controller/dto/RegistrationResponse.java
+++ src/main/java/com/womakerscode/microservicemeetups/controller/dto/RegistrationResponse.java
@@ -7,9 +7,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/com/womakerscode/microservicemeetups/model/entity/Event.java
+++ src/main/java/com/womakerscode/microservicemeetups/model/entity/Event.java
@@ -6,7 +6,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.List;
 
